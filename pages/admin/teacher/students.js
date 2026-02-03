99b2f98b5b23acd392a775f64e8fdc6905352aec
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-02-04 00:41:49 +0200

    Update and rename pages/admin/students.js to pages/admin/teacher/students.js

--- pages/admin/teacher/students.js
+++ pages/admin/teacher/students.js
@@ -1,4 +1,4 @@
-import TeacherLayout from '../../components/TeacherLayout';
+import TeacherLayout from '../../../components/TeacherLayout';
 import { useState, useEffect } from 'react';
 
 export default function StudentsPage() {
