c651fce1132b5f8930a7d359673689aed05f2875
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-02-04 15:12:20 +0200

    Update super-login-as.js

--- pages/api/auth/super-login-as.js
+++ pages/api/auth/super-login-as.js
@@ -1,5 +1,5 @@
-import { supabase } from '../../../lib/supabaseClient';
-import { requireSuperAdmin } from '../../../lib/dashboardHelper';
+import { supabase } from '../../lib/supabaseClient';
+import { requireSuperAdmin } from '../../lib/dashboardHelper';
 import { serialize } from 'cookie';
 import crypto from 'crypto';
 
