30e19f7287773df670506999605d2bd6e7e4b83f
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-02-05 13:59:15 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,10 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
       <Head>
-        {/* تم تغيير الرابط إلى الرابط الرسمي والمباشر من تليجرام.
-          هذا هو الحل المضمون لمشكلة عدم تحميل السكريبت.
-        */}
-        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+        {/* تم إزالة سكريبت تليجرام لتحسين الأمان */}
       </Head>
       <body>
         <Main />
