e8df935736fb19d059229c6cdd2f63e00234617d
medaad5682 <medaad5682@gmail.com>
2026-07-09 14:17:58 +0300

    Update get-app-init-data.js

--- pages/api/public/get-app-init-data.js
+++ pages/api/public/get-app-init-data.js
@@ -289,7 +289,7 @@ export default async (req, res) => {
           telegram: contactInfo['support_telegram'] || ''
       },
       // ✅ إرسال حالة الوضع المجاني
-      freeModeV3: contactInfo['free_mode'] === 'true'
+      freeModeV4: contactInfo['free_mode'] === 'true'
     });
 
   } catch (err) {
