18730b4fc2269cf3a61d923adbe647e482d5f32f
medaad5682 <medaad5682@gmail.com>
2026-09-07 01:05:39 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
         {/* تم إزالة سكريبت تليجرام لتحسين الأمان */}
       </Head>
       <body>
